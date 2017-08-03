fd04ea2b2c0394c1f104bec428cf8b35e1cf356d
Emilien Chapon <emilien.chapon@cern.ch>
2017-08-03 17:03:17 +0200

    Add missing file

---
+++ MCFM-8.0/src/Inc/npdf.f
@@ -0,0 +1,5 @@
+      Integer :: Aw, openchannelw, orderw, psetw, inidssz,choosew,iwrp
+      common/npdf_int/Aw, openchannelw, orderw, psetw,
+     - inidssz,choosew,iwrp
+      character*20 nPDFname
+      common/npdf_char/nPDFname
